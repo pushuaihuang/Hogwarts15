aaa1c85acc1f74f91c45f7a24746dad50844a652
pushuaihuang <1310721876@qq.com>
2020-10-21 10:01:52 +0800

    测试数据数据驱动

--- testing/test_param.py
+++ testing/test_param.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 #coding=utf-8
+import pytest
+#笛卡尔
+# @pytest.mark.parametrize("b",[1,2,3])
+# @pytest.mark.parametrize("a",[1,2,3])
+# def test_para(a,b):
+#     print(a,b)
+
